8aa10461c76a4079c5198d40149ea05e3a9d4171
Balazs Szamody <bszamody@gmail.com>
2022-02-27 16:08:29 +1100

    Initial Commit

---
+++ CollectionListView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionListView
+//
+//  Created by Balázs Szamódy on 27/2/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
