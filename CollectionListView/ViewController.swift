e8513d1d449724aff9ff953e1a6016d2a6019d82
Balazs Szamody <bszamody@gmail.com>
2022-04-09 10:00:27 +1000

    Header and Footer

--- CollectionListView/ViewController.swift
+++ CollectionListView/ViewController.swift
@@ -43,7 +43,9 @@ class ViewController: UIViewController {
 
     private func buildCollectionView() -> UICollectionView {
         var config = UICollectionLayoutListConfiguration(appearance: .plain)
-
+        config.headerMode = .supplementary
+        config.footerMode = .supplementary
+        
         let layout = UICollectionViewCompositionalLayout.list(using: config)
 
         return UICollectionView.init(frame: .zero, collectionViewLayout: layout)
@@ -51,9 +53,22 @@ class ViewController: UIViewController {
 
     private func buildDataSource() -> UICollectionViewDiffableDataSource<Section, Item> {
         let cellRegistration = buildCellRegistration()
-        return UICollectionViewDiffableDataSource(collectionView: collectionView) { collectionView, indexPath, itemIdentifier in
+        let headerRegistration = buildHeaderRegistration()
+        let footerRegistration = buildFooterRegistration()
+
+        let dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView) { collectionView, indexPath, itemIdentifier in
             collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
         }
+
+        dataSource.supplementaryViewProvider = { collectionView, elementKind, indexPath in
+            if elementKind == UICollectionView.elementKindSectionHeader {
+            return collectionView.dequeueConfiguredReusableSupplementary(using: headerRegistration, for: indexPath)
+            } else {
+                return collectionView.dequeueConfiguredReusableSupplementary(using: footerRegistration, for: indexPath)
+            }
+        }
+
+        return dataSource
     }
 
     private func buildCellRegistration() -> UICollectionView.CellRegistration<UICollectionViewListCell, Item> {
@@ -61,6 +76,36 @@ class ViewController: UIViewController {
             var contentConfig = cell.defaultContentConfiguration()
             contentConfig.text = itemIdentifier.text
             cell.contentConfiguration = contentConfig
+        }
+    }
+
+    private func buildHeaderRegistration() -> UICollectionView.SupplementaryRegistration<UICollectionViewListCell> {
+        UICollectionView.SupplementaryRegistration(elementKind: UICollectionView.elementKindSectionHeader) { [weak self] supplementaryView, _, indexPath in
+            guard let self = self else {
+                return
+            }
+
+            let section = self.dataSource.snapshot().sectionIdentifiers[indexPath.section]
+
+            var contentConfig = supplementaryView.defaultContentConfiguration()
+            contentConfig.text = section.title
+            supplementaryView.contentConfiguration = contentConfig
+        }
+    }
+
+    private func buildFooterRegistration() -> UICollectionView.SupplementaryRegistration<UICollectionViewListCell> {
+        UICollectionView.SupplementaryRegistration(elementKind: UICollectionView.elementKindSectionFooter) { [weak self] supplementaryView, _, indexPath in
+			var config = supplementaryView.defaultContentConfiguration()
+
+			if let sectionCount = self?.dataSource.snapshot().sectionIdentifiers.count, indexPath.section == sectionCount - 1 {
+				// Add footer for last section
+				config.text = self?.viewModel.footer
+				config.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 8, leading: 0, bottom: 16, trailing: 0)
+			} else {
+				config.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 8, leading: 0, bottom: 0, trailing: 0)
+			}
+
+            supplementaryView.contentConfiguration = config
         }
     }
 
--- CollectionListView/ViewModel.swift
+++ CollectionListView/ViewModel.swift
@@ -19,23 +19,43 @@ class ViewModel {
         var text: String
     }
 
-    let items: [Section: [Item]] = [
-        Section(title: "First") : [
+    let sections = [
+        Section(),
+        Section(title: "Second"),
+		Section()
+    ]
+
+    let items = [
+        [
             "First",
             "Second",
             "Third"
-        ]
-    ].mapValues({ $0.map({ Item(text: $0) }) })
+        ],
+        [
+            "First",
+            "Second",
+            "Third"
+        ],
+        [
+            "First",
+            "Second",
+            "Third"
+        ],
+    ].map({ $0.map({ Item(text: $0) }) })
 
     var snapshot: NSDiffableDataSourceSnapshot<Section, Item> {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
 
-        items.forEach { section, items in
+        zip(sections, items).forEach { section, items in
             snapshot.appendSections([section])
             snapshot.appendItems(items, toSection: section)
         }
 
         return snapshot
     }
+
+	var footer: String {
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+	}
 
 }
